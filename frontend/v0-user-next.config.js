87facfdd925e69c1594bf4c2e0df8771d696545c
mjahmad <mjahmad2024@outlook.com>
2025-03-09 09:22:36 -0700

    Initial TypeScript project

---
+++ frontend/v0-user-next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["mj-ahmad.github.io", "www.nusuk.sa"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mj-ahmad.github.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.nusuk.sa",
+        pathname: "/**",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
+
